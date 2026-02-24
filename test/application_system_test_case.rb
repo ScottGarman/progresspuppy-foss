46615e21341c545d2850ac9ae10313f05933571f
Scott Garman <sgarman@zenlinux.com>
2026-02-24 09:38:53 -0800

    Simplify the selenium configuration - now requires CHROMEDRIVER_BIN env

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,17 +1,15 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  # Set the driver path before driven_by so Rails' preload step skips
-  # Selenium Manager (which hangs on this system).
-  Selenium::WebDriver::Chrome::Service.driver_path =
-    ENV.fetch('CHROMEDRIVER_BIN', '/home/sgarman/bin/chromedriver')
+  # Set CHROMEDRIVER_BIN to bypass Selenium Manager's driver auto-detection,
+  # which can hang on some systems.
+  Selenium::WebDriver::Chrome::Service.driver_path = ENV['CHROMEDRIVER_BIN'] if ENV['CHROMEDRIVER_BIN']
 
   # For headless mode (CI), set HEADLESS=1: HEADLESS=1 bin/rails test:system
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400] do |driver_option|
     driver_option.add_argument('--headless=new') if ENV['HEADLESS'] || ENV['CI']
     driver_option.add_argument('--disable-gpu')
     driver_option.add_argument('--no-sandbox')
-    driver_option.binary = ENV.fetch('CHROME_BIN', '/usr/bin/google-chrome-stable')
   end
 
   def log_in_as(user)
